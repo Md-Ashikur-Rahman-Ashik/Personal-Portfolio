7ec9476533eefee2acb37d5b95063ce7837b85f5
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-27 11:24:23 +0600

    Added image on skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,21 @@ const Skills = () => {
   return (
     <div className="container mx-auto mt-20">
       <h2 className="text-center text-3xl font-bold">My Skills</h2>
+      <div className="flex justify-center mt-10 items-center">
+        <div>
+          <img
+            src="https://i.ibb.co/86rV1Jd/profile-Picture.jpg"
+            alt="Profile-Picture"
+            className="rounded-xl w-1/3"
+          />
+        </div>
+        <div>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
