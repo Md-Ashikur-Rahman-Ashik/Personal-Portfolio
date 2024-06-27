956b6bcc4825ea5c9dba25b8b3996c0b15b40873
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-27 10:58:55 +0600

    Adding skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,9 @@
+const Skills = () => {
+  return (
+    <div className="container mx-auto mt-20">
+      <h2 className="text-center text-3xl font-bold">My Skills</h2>
+    </div>
+  );
+};
+
+export default Skills;
