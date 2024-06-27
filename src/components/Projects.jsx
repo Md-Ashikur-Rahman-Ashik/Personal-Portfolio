ae7cf525d460d2b783e3f4bf9aa7f27f2910e6ff
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-27 22:54:41 +0600

    Added my skills section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,14 @@ const Projects = () => {
     {
       projectName: "Fabric Fusion",
       technologies: [
-        "HTML5, CSS3, Tailwind Css, Javascript, React Js, Express Js, Node Js, MongoDB",
+        "HTML5",
+        "CSS3",
+        "Tailwind Css",
+        "Javascript",
+        "React Js",
+        "Express Js",
+        "Node Js",
+        "MongoDB",
       ],
       description:
         "I developed the Fabric Fusion Website, a web application for textile art enthusiasts to browse a curated gallery, create and manage their own projects, and potentially share their creations with the community. This project enhanced my front-end skills and provided valuable experience in integrating front-end and back-end technologies.",
@@ -13,7 +20,14 @@ const Projects = () => {
     {
       projectName: "ActifyNow",
       technologies: [
-        "HTML5, CSS3, Tailwind Css, Javascript, React Js, Express Js, Node Js, MongoDB",
+        "HTML5",
+        "CSS3",
+        "Tailwind Css",
+        "Javascript",
+        "React Js",
+        "Express Js",
+        "Node Js",
+        "MongoDB",
       ],
       description:
         "I created ActifyNow, a Volunteer Management Website designed to connect volunteers with meaningful opportunities, fostering community growth through volunteer work. This platform enables users to easily find and participate in various activities, such as environmental conservation, social outreach, and educational initiatives, promoting both personal development and community improvement.",
@@ -23,7 +37,14 @@ const Projects = () => {
     {
       projectName: "CampMed",
       technologies: [
-        "HTML5, CSS3, Tailwind Css, Javascript, React Js, Express Js, Node Js, MongoDB",
+        "HTML5",
+        "CSS3",
+        "Tailwind Css",
+        "Javascript",
+        "React Js",
+        "Express Js",
+        "Node Js",
+        "MongoDB",
       ],
       description:
         "As the sole developer of CampMed, I designed and built a comprehensive web solution to streamline the management of medical camps, facilitating scheduling, volunteer registration, and resource tracking. CampMed has reduced administrative burdens and improved the overall experience for volunteers and organizers, with future plans for additional features and mobile support.",
@@ -38,14 +59,38 @@ const Projects = () => {
       <div>
         {projects.map((project) => (
           <div
-            className="flex justify-center items-center mb-10"
+            className="flex gap-10 justify-center items-center mb-10"
             key={project.projectName}
           >
-            <div>
-              <h2 className="text-2xl">{project.projectName}</h2>
+            <div className="w-1/2">
+              <h2 className="text-2xl font-bold mb-4">{project.projectName}</h2>
+              <ul className="grid grid-cols-4 gap-2">
+                {project.technologies.map((technology, index) => (
+                  <li
+                    className="border-2 text-center rounded-xl p-1 font-bold"
+                    key={index}
+                  >
+                    {technology}
+                  </li>
+                ))}
+              </ul>
+              <p className="mt-4">{project.description}</p>
+              <div className="flex justify-center mt-4">
+                <a
+                  href={project.liveLink}
+                  className="btn bg-black text-white hover:bg-black"
+                  target="_blank"
+                >
+                  View Project
+                </a>
+              </div>
             </div>
             <div className="w-1/3">
-              <img src={project.photo} alt={project.projectName} className="rounded-xl" />
+              <img
+                src={project.photo}
+                alt={project.projectName}
+                className="rounded-xl hover:scale-105 transition-transform"
+              />
             </div>
           </div>
         ))}
