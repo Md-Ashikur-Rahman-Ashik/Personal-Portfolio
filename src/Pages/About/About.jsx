d69ad7022209881de56eab169a226f99973ced06
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-28 14:11:14 +0600

    Added resume on portfolio website

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="container mx-auto">
+      <iframe
+        src="/AshikurRahmanResume.pdf"
+        style={{ width: "1300px", height: "1100px" }}
+      />
+    </div>
+  );
+};
+
+export default About;
